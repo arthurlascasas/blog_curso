34243dd1c7a9ead72a2fdc008296f56d34f31375
Arthur <arthur.lascasas@sitextec.com.br>
2023-12-07 10:38:37 -0300

    Add layout Blog, config article

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+require("@popperjs/core")
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -9,6 +10,9 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap'
 import '../stylesheets/application'
+
+
+
 
 Rails.start()
 Turbolinks.start()
