1b3a9d4e6aa584e9f43a62f1569e754ccc98bfd1
Arthur <arthur.lascasas@sitextec.com.br>
2023-12-12 10:56:09 -0300

    Add posts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,15 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
 
   def index
+    @highlights = Article.desc_order.first(3)
+
+
     current_page = (params[:page] || 1).to_i
-    @articles = Article.order(created_at: :desc).page(current_page).per(1)
+    highlight_ids = @highlights.pluck(:id).join(',')
+
+    @articles = Article.without_highlights(highlight_ids)
+                        .desc_order
+                        .page(current_page)
   end
 
   def show
