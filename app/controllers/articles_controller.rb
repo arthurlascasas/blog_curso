a04410cef34e5bf627a27e84e06bc1b72edf054c
Arthur <arthur.lascasas@sitextec.com.br>
2024-01-03 10:18:32 -0300

    Add comments Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   include Paginable
 
   before_action :authenticate_user!, except: %i[index show]
-  before_action :set_article, only: %i[show edit update destroy]
+  before_action :set_article, only: %i[edit update destroy]
   before_action :set_categories, only: %i[new create edit update]
 
   def index
@@ -30,7 +30,8 @@ class ArticlesController < ApplicationController
   end
 
   def show
-   
+    @article = Article.includes(comments: :user).find(params[:id])
+    authorize @article
   end
 
 
